c117a5de5720581fa39ea79acea4904dfb8665da
aosorio <argenisosorio580@hotmail.com>
2018-07-23 10:05:47 -0400

    Initial commit

---
+++ base/constants.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+areas = (
+    ('Seleccione','Seleccione'),
+    ('Programación','Programación'),
+    ('Diseño Gráfico','Diseño Gráfico'),
+    ('Redacción','Redacción'),
+    ('Traducción','Traducción'),
+    ('Edición Multimedia','Edición Multimedia'),
+    ('Marketing Digital','Marketing Digital'),
+    ('Soporte Administrativo y Legal','Soporte Administrativo y Legal'),
+    ('Análisis de datos','Análisis de datos'),
+    ('Ingeniería y Arquitectura','Ingeniería y Arquitectura'),
+    ('Business','Business'),
+)
